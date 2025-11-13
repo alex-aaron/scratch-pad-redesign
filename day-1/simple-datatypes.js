a6584aabf9b8c76c611c451b5b69cd4b3f4c538d
Alex Aaron <alexfrancisaaron@gmail.com>
2025-11-13 12:47:51 -0600

    added first 7 simple-datatypes prompts

---
+++ day-1/simple-datatypes.js
@@ -0,0 +1,105 @@
+//////////////////////////////
+// Simple Datatypes Prompts //
+//////////////////////////////
+
+
+/**
+ * Directions: Create a function called `toLowerCase` that takes
+ * in an input string. This function should return the input
+ * string lowercased.
+ */
+function toLowerCase(string){
+  
+}
+
+/**
+ * Directions: Create a function called `toUpperCase` that takes
+ * in an input string. This function should return the input
+ * string uppercased
+ */
+function toUpperCase(string){
+  
+}
+
+/**
+ * Directions: Create a function called `beginsWith` that takes
+ * in an input string and a letter. This function should return true
+ * if the input string begins with the input letter. 
+ * 
+ * Note: This function should be case-insensitive.
+ * 
+ * Example:
+ * 
+ *    beginsWith('hello', 'H'); // true
+ *    beignsWith('World', 'V'); // false
+ */
+
+function beginsWith(string){
+
+}
+
+/**
+ * Directions: Create a function called `endsWith` that takes
+ * in an input string and a letter. This function should return true
+ * if the input string ends with the input letter. 
+ * 
+ * Note: This function should be case-insensitive.
+ * 
+ * Example:
+ * 
+ *    endsWith('world', 'D'); // true
+ *    endswith('world, 'e'); // false
+ */
+
+function endswith(string){
+
+}
+
+/**
+ * Directions: Create a function called `getFirstWord` that takes in a string
+ * that will always have at least 2 words in it. This function should use
+ * the split method to return the first word in the string.
+ * 
+ * Example:
+ * 
+ *    getFirstWord('hello world'); // => 'hello'
+ *    getFirstWord('first day at operation spark'); // => 'first'
+ */
+
+function getFirstWord(string){
+
+}
+
+/**
+ * Directions: Create a function called `toDashCase` that takes in
+ * a string. This function should return the input string lowercased
+ * with dashes between each word. Use the split and join method in your
+ * implementation.
+ * 
+ * Example:
+ * 
+ *    toDashCase('Hello World'); // => 'hello-world'
+ */
+
+function toDashCase(string){
+
+}
+
+/**
+ * Directions: Create a function called `getWholeNumber` that takes in a decimal
+ * point number as its input. This function should return the nearest whole number
+ * rounded down.
+ * 
+ * Example:
+ *    
+ *    getWholeNumber(9.567); // => 9
+ * 
+ * Note: There are a variety of ways to solve this problem and a variety of methods
+ * you can use. Fill out your reflection after solving to describe the approach you
+ * took and why.
+ */
+
+function getWholeNumber(n){
+
+}
+
